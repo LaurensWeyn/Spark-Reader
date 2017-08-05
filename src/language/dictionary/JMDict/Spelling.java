29ced69199571a4d72a0477718fd432b6a1f6429
wareya <wareya@gmail.com>
2017-08-05 02:19:12 -0400

    Initial reimplementation of FrequencySink logic

--- src/language/dictionary/JMDict/Spelling.java
+++ src/language/dictionary/JMDict/Spelling.java
@@ -2,8 +2,7 @@ package language.dictionary.JMDict;
 
 import language.dictionary.DefTag;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Holds both Kanji and Kana versions of a word.
@@ -14,6 +13,7 @@ public class Spelling
     private boolean isKanji;
     private String spelling;
     private Spelling[] dependencies;
+    private List<Spelling> readings;
     private Set<DefTag> tags;
 
     public Spelling(boolean isKanji, String spelling)
@@ -21,6 +21,7 @@ public class Spelling
         this.isKanji = isKanji;
         this.spelling = spelling;
         dependencies = null;
+        readings = null;
         tags = null;
     }
 
@@ -33,6 +34,18 @@ public class Spelling
     {
         if(tags == null)tags = new HashSet<>();
         tags.add(tag);
+    }
+    
+    public void addReading(Spelling reading)
+    {
+        if(readings == null)readings = new LinkedList<>();
+        readings.add(reading);
+    }
+    
+    public List<Spelling> getReadings()
+    {
+        if(readings == null) return Arrays.asList(this); // ugly hack? fix later? for kana-only spellings with no attached reading
+        else return readings;
     }
 
     public boolean isKanji()
